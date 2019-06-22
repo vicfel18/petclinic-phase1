9b216ebc2fc5e5ae63b4a0d425fdf681203dde41
Spyro1001 <raymondhedgecock@gmail.com>
2019-06-22 11:31:34 -0400

    adding README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# PetClinic Phase 1
+
+###### Overview
+In this phase of the project, you create a backend for the PetClinic application based on the existing PetClinic website at 
+
+http://petclinic.cognizantacademy.com
+
+###### What You Are Given
+You are given the following:
+- Code for the `OwnerController`
+- Code for the `OwnerService`
+- Code for the `OwnerRepository`
+- A `FakeDatabase`
+Supporting interfaces for the `FakeDatabase` (`Modifiable` and `CrudRepository`)
+
+###### What You Need To Complete
+- Complete the design and implement classes for the rest of the models (`Pet`, `Visit`, `Vet`).
+- Implement controllers, services and repositories for each of the models.
+
+Use the following patterns to complete this phase of the code:
+
+- MVC
+- 3-Tier application
+- Polymorphism (inheritance)
+- Designing to interfaces
+
+###### Notes
+
+- Use the existing code as a pattern for the rest of your code.
+- In this phase, don't worry about handling errors.
--- src/main/java/com/example/petclinic/PetClinicDriver.java
+++ src/main/java/com/example/petclinic/PetClinicDriver.java
@@ -11,38 +11,45 @@ import java.util.Date;
 import java.util.List;
 
 
-public class ManualDriver {
+public class PetClinicDriver {
 
     public static void main(String[] args) {
 
-        // Owner dependency injection setup
+        // Owner dependency injection (DI) setup
         OwnerRepository ownerRepository = new OwnerRepository();
         OwnerService ownerService = new OwnerService(ownerRepository);
         OwnerController ownerController = new OwnerController(ownerService);
 
         // TODO add other 3-tier DI setups here
 
-        // Owner testing
+        // ***** Owner testing *****
+
+        // create our owners
         Owner owner1 = new Owner(1, "Homer Simpson", "742 Evergreen Terrace", "Springfield", "9395550113");
         Owner owner2 = new Owner(2, "Marge Simpson", "742 Evergreen Terrace", "Springfield", "9395550113");
         Owner owner3 = new Owner(3, "Lisa Simpson", "742 Evergreen Terrace", "Springfield", "9395550113");
         Owner owner4 = new Owner(4, "Bart Simpson", "742 Evergreen Terrace", "Springfield", "9395550113");
 
+        // save owners to database
         ownerController.saveOwner(owner1);
         ownerController.saveOwner(owner2);
         ownerController.saveOwner(owner3);
         ownerController.saveOwner(owner4);
 
+        // get all owners from database and display them
         List<Owner> owners = ownerController.getAllOwners();
-
         display(owners);
 
+        // create some pets and add them to an existing owner
         Pet pet1 = new Pet(1, "Godzilla", new Date(), PetType.LIZARD);
         Pet pet2 = new Pet(2, "Santa's Little Helper", new Date(), PetType.DOG);
         owner4.addPet(pet1);
         owner4.addPet(pet2);
 
+        // display the owner info again
         display(owners);
+
+        // TODO add other tests for other models here
     }
 
     /**
